f586e45addb0f444ea0dcc132c0c0c8ad70e7054
pk <yellowdayo@naver.com>
2024-11-15 21:30:07 +0900

    test

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,42 @@
 import { useEffect } from "react";
 import supabase from "../supabase";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const PostboxWrapper = styled.div`
+  background-color: #ffe4b8;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 5%;
+  padding: 10px;
+`;
+
+const Postbox = styled.div`
+  background-color: yellow;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 0;
+`;
+
+const PostboxImage = styled.img`
+  background-color: grey;
+  width: 500px;
+  height: 300px;
+`;
+
+const PostboxTitle = styled.button`
+  background-color: orange;
+`;
 
 const Main = () => {
+  const navigate = useNavigate();
   useEffect(() => {
     const fetchData = async () => {
-      const { data, error } = await supabase.from("id").select("*");
+      const { data, error } = await supabase.from("posts").select("*");
 
       if (error) {
         console.error("데이터 가져오기 오류:", error);
@@ -15,7 +47,22 @@ const Main = () => {
     fetchData();
   }, []);
 
-  return <div>Main</div>;
+  return (
+    <>
+      <PostboxWrapper>
+        <Postbox>
+          <PostboxImage />
+          <PostboxTitle
+            onClick={() => {
+              navigate(`/detail`);
+            }}
+          >
+            포스트 제목
+          </PostboxTitle>
+        </Postbox>
+      </PostboxWrapper>
+    </>
+  );
 };
 
 export default Main;
