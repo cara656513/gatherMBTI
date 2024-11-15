af6fbdfc2da56eacf4051162e540ba39cfda851a
pk <yellowdayo@naver.com>
2024-11-15 19:22:31 +0900

    test

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,21 @@
+import { useEffect } from "react";
+import supabase from "../supabase";
+
 const Main = () => {
-  return <div>Main!</div>;
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("id").select("*");
+
+      if (error) {
+        console.error("데이터 가져오기 오류:", error);
+      }
+      console.log("Supabase 데이터:", data);
+    };
+
+    fetchData();
+  }, []);
+
+  return <div>Main</div>;
 };
 
 export default Main;
