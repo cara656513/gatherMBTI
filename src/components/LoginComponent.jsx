75226329f2bf98f561c6352b2cf6723f44c20b10
강란규 <bpos333@gmail.com>
2024-11-18 20:09:49 +0900

    로그인기능

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -6,6 +6,7 @@ const LoginComponent = () => {
      const [email , setemail] = useState(""); 
      const [password , setpassword ] = useState(""); 
      const navigate = useNavigate();
+     const [errorMessage, setErrorMessage] = useState("");
      const onChangeEmail = (e) => {
       setemail(e.target.value);
     };      
@@ -22,25 +23,35 @@ const LoginComponent = () => {
       })
       if (error) {
         console.error("로그인 실패:", error.message);
-      } else {
+         console.log(error)
+        }
+       else {
         console.log("로그인 성공:", data);
-        setuser(data.user); // 로그인 성공 시 사용자 정보 설정
+        setErrorMessage("")
+        navigate('/')
       }
-     navigate ( "/");
       
     
   
     }   
 
   return (
+    
     <form action="" onSubmit = {signInUser} > 
     <span>이메일</span>
     <input type="text" placeholder='이메일을 입력하세요'  value={email} onChange={onChangeEmail} />
     <span>비밀번호</span>
     <input type="password" placeholder='비밀번호를 입력하세요' value={password} onChange={onChangePassword}/>
     <button type='subimt' >로그인</button>
+    { <p style={{ color: "red" }}>{errorMessage}</p>}
     </form>
   )
 }
 
 export default LoginComponent
+
+
+
+
+
+
