ffb3e62ff2a2a08fcf6d748ed4fb72d85dfffd06
jeongeun <ddo_eun@naver.com>
2024-11-19 16:51:17 +0900

    feature:detail

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -3,8 +3,20 @@ import reset from "styled-reset";
 
 export const GlobalStyle = createGlobalStyle`
   ${reset}
-    a{
-        text-decoration-line: none;
-        color: black
-    }
+  a{
+    text-decoration-line: none;
+    color: black
+  }
+  
+    
+  @font-face {
+    font-family: 'KBO-Dia-Gothic_light';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-2@1.0/KBO-Dia-Gothic_light.woff') format('woff');
+    font-weight: 300;
+    font-style: normal;
+  
+  }
+
+  body {font-family: 'KBO-Dia-Gothic_light';}
+
 `;
