006cbc9db1278adbb46ad9085a84a9f4e92f97d1
최혜진 <choihyejin94@naver.com>
2024-11-19 10:46:55 +0900

    Feat: 스토리지에 이미지저장 및 불러오기 구현완료

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import supabase from "../supabase";
 import {
   AccountInfor,
@@ -21,6 +22,7 @@ import {
 } from "../styles/SignUpStyles";
 
 const SignUp = () => {
+  const navigate = useNavigate();
   const [users, setUsers] = useState([]);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -60,6 +62,39 @@ const SignUp = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (password.length < 8) {
+      alert("비밀번호 8자 이상 입력해 주세요.");
+      return;
+    }
+
+    if (password !== passWordConfirm) {
+      alert("비밀번호가 일치하지 않습니다.");
+      return;
+    }
+
+    const { data: basicData } = supabase.storage
+      .from("profile_img")
+      .getPublicUrl("default-profile.jpg");
+
+    let profileImgUrl = basicData.publicUrl;
+
+    if (profileImg) {
+      const { data: uploadData, error: uploadError } = await supabase.storage
+        .from("profile_img")
+        .upload(`profiles/${Date.now()}_${profileImg.name}`, profileImg);
+
+      if (uploadError) {
+        alert(uploadError.message);
+        return;
+      }
+
+      const { data } = supabase.storage
+        .from("profile_img")
+        .getPublicUrl(uploadData.path);
+      profileImgUrl = data.publicUrl;
+    }
+
     const { data: userData, error: userError } = await supabase.auth.signUp({
       email,
       password,
@@ -76,7 +111,7 @@ const SignUp = () => {
         {
           name,
           nickname: nickName,
-          profile_img: profileImg,
+          profile_img: profileImgUrl,
           profile_text: profileText,
           mbti,
         },
@@ -88,21 +123,9 @@ const SignUp = () => {
     }
     setUsers([...users, ...data]);
 
-    const { data: storageData } = supabase.storage
-      .from("avatars")
-      .getPublicUrl("default-profile.jpg");
-
-    if (password.length < 8) {
-      alert("비밀번호 8자 이상 입력해 주세요.");
-      return;
-    }
-
-    if (password !== passWordConfirm) {
-      alert("비밀번호가 일치하지 않습니다.");
-      return;
-    }
-
     alert("회원가입이 완료되었습니다.");
+
+    navigate("/");
 
     setEmail("");
     setPassword("");
@@ -127,7 +150,11 @@ const SignUp = () => {
         <p>회원가입</p>
         <ImgLabel htmlFor="fileInput">
           {profileImg ? (
-            <PlusImgStyle id="fileInput" src={profileImg} alt="ProfileImg" />
+            <PlusImgStyle
+              id="fileInput"
+              src={URL.createObjectURL(profileImg)}
+              alt="ProfileImg"
+            />
           ) : (
             "+"
           )}
@@ -238,7 +265,7 @@ const SignUp = () => {
           onChange={handleProfileTextChange}
         />
       </label>
-      <SignUpButton>가입하기</SignUpButton>
+      <SignUpButton type="submit">가입하기</SignUpButton>
     </Container>
   );
 };
