bca109f00c69be9f58a5ba7303586682980e223e
강란규 <bpos333@gmail.com>
2024-11-19 21:09:23 +0900

    로그아웃 기능

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,24 @@
+import { Navigate, useNavigate } from "react-router-dom";
 import { Header } from "../components/Header";
 import LoginComponent from "../components/LoginComponent";
 
 
 const Login = () => {
+  const navigate = useNavigate () ; 
+  const Logout = async() => {
+    const { data, error } = await supabase.auth.signOut();
+    console.log("signout: ", { data, error }); // data는 딱히 필요없을 듯
+    navigate  = ("/login")
+  }
+
   return (
     <>
       <Header menus={[
         { route: "/signup", menu: "회원가입" },
+        
+       
       ]}
+         Logout={Logout}
       />
 
       <LoginComponent />
