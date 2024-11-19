4e407a52063751be06f6017cfcba87bf77e71020
cara <cara656513@gmail.com>
2024-11-19 12:57:08 +0900

    fix: 로그인 헤더 추가

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,15 +2,17 @@ import { Header } from "../components/Header";
 import LoginComponent from "../components/LoginComponent";
 
 const Login = () => {
-  return(
+  return (
     <>
-    
-      <LoginComponent/>
-      </>
-
-
-  ) 
- 
+      <Header
+        menus={[
+          { route: "/newpost", menu: "글쓰기" },
+          { route: "/mypage", menu: "마이 페이지" },
+        ]}
+      />
+      <LoginComponent />
+    </>
+  );
 };
 
 export default Login;
